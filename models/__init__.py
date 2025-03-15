363d9c82d46115da211dfaa3ae54622771582c59
Wenliang <wmz5132@psu.edu>
2025-03-15 19:07:36 -0400

    add llava next model support

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,12 @@
 from .base import BaseModel
 from .internvl2 import Internvl2
 from .llama32 import Llama32
+from .llava_next import LlavaNext
+
 
 __all__ = [
     "BaseModel",
     "Internvl2",
     "Llama32",
+    "LlavaNext",
 ]
