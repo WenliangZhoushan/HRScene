2186cc8219489ab65d86449d83fa2c0a6d50d076
Wenliang <wmz5132@psu.edu>
2025-03-19 02:14:42 -0400

    update example

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ import torch
 
 
 # Example 1: Run 150 complexgrid_3x3 samples on local model
-model = Llama32(model_path="/scratch1/wmz5132/models/huggingface/Llama-3.2-11B-Vision-Instruct", torch_dtype=torch.bfloat16, device_map="cuda")
+model = Llama32(model_path="meta-llama/Llama-3.2-11B-Vision-Instruct", torch_dtype=torch.bfloat16, device_map="cuda")
 tester = DiagnosisTester(model=model, dataset_name="complexgrid_3x3", num_samples=150)
 tester.run()
 tester.eval()
